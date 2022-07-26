e5ef7f7b9abba3975e492e729c5d540887bcb633
mesinkasir <hockeycorpmarketing@gmail.com>
2022-07-26 13:30:48 +0700

    2 files modified, 1 deleted

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,46 @@
 /** @type {import('next-sitemap').IConfig} */
+
 const config = {
-  siteUrl: process.env.SITE_URL || 'https://onlinemesinkasir.pages.dev/',
+  siteUrl: 'https://onlinemesinkasir.pages.dev/',
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
   generateRobotsTxt: true,
+  alternateRefs: [
+    {
+      href: 'https://onlinemesinkasir.pages.dev/',
+      hreflang: 'id',
+    },
+    {
+      href: 'https://onlinemesinkasir.pages.dev/demo',
+      hreflang: 'id',
+    },
+    {
+      href: 'https://onlinemesinkasir.pages.dev/about',
+      hreflang: 'id',
+    },
+  ],
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
+  additionalPaths: async (config) => [
+    await config.transform(config, '/additional-page'),
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
 };
 
 export default config;
